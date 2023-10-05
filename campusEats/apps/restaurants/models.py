d387dcf9f4075daaaa00714d70af2fe3be9cba89
Olivia <osmi3220@uni.sydney.edu.au>
2023-10-05 14:05:58 +1100

    implemented models for: restaurant, user, faovuriteRestaurant, bookmark

--- campusEats/apps/restaurants/models.py
+++ campusEats/apps/restaurants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    RestaurantID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255, null = False)
+    Location = models.CharField(max_length=255, null = True)
+    Description = models.CharField(max_length=255, null = True)
+    ImageURL = models.CharField(max_length=255, null = True)
+
+
--- campusEats/apps/user/models.py
+++ campusEats/apps/user/models.py
@@ -1,2 +1,35 @@
-# from django.contrib.auth.models import AbstractUser
+from django.db import models
+from enum import Enum
 
+from restaurants.models import Restaurant
+
+class UserType(Enum):
+    USER = 'user'
+    ADMIN = 'admin'
+
+class User(models.Model):
+    UserID = models.AutoField(primary_key = True)
+    UserName = models.CharField(max_length=255, null = False)
+    Password = models.CharField(max_length=255, null = False)
+    Email = models.CharField(max_length=255)
+    Phone = models.CharField(
+        max_length=20,
+        validators=[RegexValidator(r'^[0-9]*$', 'Only numeric characters are allowed.')],
+    )
+    Role = models.CharField(
+        max_length=5,
+        choices=[(tag.name, tag.value) for tag in UserType]
+    )
+    Bookmark = models.ForeignKey(Bookmark, on_delete=models.CASCADE)
+
+class Bookmark(models.Model):
+    BookmarkID = models.AutoField(primary_key = True)
+    RestaurantID = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+class FavRestaurants(models.Model):
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
+    RestaurantID = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['UserID', 'RestaurantID'], name='unique_favourite_key')
+        ]
