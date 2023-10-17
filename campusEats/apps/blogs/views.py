3c974e5681357ccba42d8e6b1ca0363e56b98d1d
Yinglan Li <yili9597@uni.sydney.edu.au>
2023-10-17 19:27:09 +1100

    bug fixed

--- campusEats/apps/blogs/views.py
+++ campusEats/apps/blogs/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
-# Create your views here.
+
+def blog_test(request):
+    return render(request, 'blogs/test.html')
