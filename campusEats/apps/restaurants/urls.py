8b4d630c6a54f90f64c274bc22a79965c94f642c
VivianH <weha7612@uni.sydney.edu.au>
2023-10-26 23:34:56 +1100

    ajax listing pg "load more"

--- campusEats/apps/restaurants/urls.py
+++ campusEats/apps/restaurants/urls.py
@@ -1,6 +1,7 @@
 # user/urls.py
 from django.urls import path, include, re_path as url
 from . import views
+from .views import MainView, RestaurantJsonListview
 
 urlpatterns = [
     # Other URL patterns for user-related views
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', views.render_home, name='Home'),
     path('restaurant/<int:restaurant_id>/', views.restaurant_detail, name='restaurant_detail'),
     path('search/all', views.restaurant_list_view, name='restaurant_list'),
+    path('', MainView.as_view(), name='main-view'),
+    path('restaurants-json/<int:restaurant_id>/', RestaurantJsonListview.as_view(), name='restaurants-json-view'),
 ]
