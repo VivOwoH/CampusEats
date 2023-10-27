c63122c07e22aa9d41b399a7324924f2c7ce3c80
VivianH <weha7612@uni.sydney.edu.au>
2023-10-27 12:15:53 +1100

    tmp

--- campusEats/apps/cloudinary_integration/views.py
+++ campusEats/apps/cloudinary_integration/views.py
@@ -10,12 +10,12 @@ from django.shortcuts import redirect
 
 def upload_view(request):
     if request.method == "POST":
+
         form = ImageUploadForm(request.POST, request.FILES)
 
         if request.FILES['image'].size > 10485760:  # 10 MB in bytes
             # Handle the error, e.g., return an error message to the user
             return HttpResponse("The image file is too large. Please upload an image less than 10 MB.")
-
 
         if form.is_valid():
             # Upload image to Cloudinary
@@ -23,20 +23,21 @@ def upload_view(request):
             image_url = response['url']
 
             # Get the model and its ID from the POST data
-            model_name = request.POST.get('model_name')  # e.g., 'blog', 'review', 'comment'
-            model_id = request.POST.get('model_id')
+            # model_name = request.POST.get('model_name')  # e.g., 'blog', 'review', 'comment'
+            # model_id = request.POST.get('model_id')
 
             # Get the ContentType for the given model
-            content_type = ContentType.objects.get(model=model_name.lower())
+            # content_type = ContentType.objects.get(model=model_name.lower())
 
             # Create the UploadedImage instance
             UploadedImage.objects.create(
                 image_url=image_url,
-                content_type=content_type,
-                object_id=model_id
+                # content_type=content_type,
+                # object_id=model_id
             )
 
             return redirect(image_url)  # Redirect to a success page or another view
+        
     else:
         form = ImageUploadForm()
 
